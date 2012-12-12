252ec09d74c047b71296d390ee626ea7abfb73f4
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2012-12-12 16:16:51 +0100

    Supprime duplication

--- katas/recherche-dichotomique/node.js/recherche.js
+++ katas/recherche-dichotomique/node.js/recherche.js
@@ -2,13 +2,12 @@ exports.recherche = function (item, liste) {
   var borneMax, pivot;
 
   borneMax = liste.length - 1;
-  pivot = (borneMax) / 2;
 
   while (borneMax >= 0) {
+    pivot = (borneMax) / 2;
     if (liste[pivot] === item ) { return pivot; }
 
     borneMax = pivot - 1;
-    pivot = (borneMax) / 2;
   }
 
   return -1;
