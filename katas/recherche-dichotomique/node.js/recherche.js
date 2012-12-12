b45b75c4308fb9bf0a1452936eae75cfb2f8f1f3
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2012-12-12 15:56:33 +0100

    Recherche dans liste à un élément

--- katas/recherche-dichotomique/node.js/recherche.js
+++ katas/recherche-dichotomique/node.js/recherche.js
@@ -1,3 +1,7 @@
-exports.recherche = function () {
+exports.recherche = function (item, liste) {
+  if (liste.length === 0) { return -1; }
+
+  if (liste[0] === item ) { return 0; }
+
   return -1;
 };
--- katas/recherche-dichotomique/node.js/test.js
+++ katas/recherche-dichotomique/node.js/test.js
@@ -8,4 +8,14 @@ describe("Ma fonction recherche", function () {
       assert.equal(-1, recherche.recherche(3, []));
     });
   });
+
+  describe("sur une liste à un élément", function () {
+    it("retourne 0 pour l'élément de la liste", function () {
+      assert.equal(0, recherche.recherche(3, [3]));
+    });
+
+    it("retourne -1 pour tous les autres cas", function () {
+      assert.equal(-1, recherche.recherche(1, [3]));
+    });
+  });
 });
