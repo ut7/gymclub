d4cf4f610f6844452ae6d04cdbf9bf487e32a9c0
Laurent Arnoud <laurent@spkdev.net>
2013-01-25 18:10:26 +0100

    Calcul price.
    Signed-off-by: Laurent arnoud <laurent.arnoud@af83.com>
    Signed-off-by: Paul-Xavier Ceccaldi <paul-xavier.ceccaldi@af83.com>

--- katas/Kata-potter/130118_pix/potter.js
+++ katas/Kata-potter/130118_pix/potter.js
@@ -1,14 +1,22 @@
+var reducs = [
+  1,
+  0.95,
+  0.90,
+  0.80,
+  0.75
+];
+
 function sort(a) {
   return a.sort();
 }
 
-function lot(a) {
+function lots(a) {
   var lots = [];
   while (!isMinus1(a)) {
     var max = -1;
     var q = [];
-    for(var i = 0; i < a.length; i++) {
-      if ( a[i] > max ) {
+    for (var i = 0; i < a.length; i++) {
+      if (a[i] > max) {
         q.push(a[i]);
         max = a[i];
         a[i] = -1;
@@ -20,7 +28,7 @@ function lot(a) {
 }
 
 function isMinus1(a) {
-  for(var i = 0; i < a.length; i++) {
+  for (var i = 0; i < a.length; i++) {
     if (a[i] !== -1) {
       return false;
     }
@@ -28,6 +36,26 @@ function isMinus1(a) {
   return true;
 }
 
+function price(results) {
+  var price = 0;
+  for (var i = 0; i < results.length; i++) {
+    var element = results[i];
+    var reduc = reducs[element.length - 1];
+    price += (8 * element.length * reduc);
+  }
+  return price;
+}
+
+function hasThreeAndFive(results) {
+  var indexes = [[], []];
+  for (var i = 0; i < results.length; i++) {
+     if (results[i].length === 3)
+         indexes[0].push(i);
+     if (results[i].length === 5)
+         indexes[1].push(i);
+  }
+  return indexes;
+}
 
 var random = [];
 while (random.length < 20) {
@@ -36,4 +64,9 @@ while (random.length < 20) {
 
 var books = random;
 console.log(random);
-console.log(lot(sort(books)));
+var results = lots(sort(books));
+console.log(results);
+threeAndFiveIndexes = hasThreeAndFive(results);
+
+var price = price(results);
+console.log(price);
