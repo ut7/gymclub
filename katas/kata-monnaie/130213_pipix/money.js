f8bd838c305b9ac49a987645972cb6a00e1ccbf5
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2013-02-20 14:53:44 +0100

    Advancing on kata-monnaie

--- katas/kata-monnaie/130213_pipix/money.js
+++ katas/kata-monnaie/130213_pipix/money.js
@@ -1,8 +1,26 @@
-var monney = [
+var COINS = [
   { name: 'or',     value: 21 },
   { name: 'argent', value: 11 },
   { name: 'bronze', value: 7 },
   { name: 'cuivre', value: 1 }
 ];
 
-var results = [];
+// sort coins by value...
+COINS = COINS.sort(function(a, b) {
+  return b.value - a.value;
+});
+
+function money(amount, coins) {
+  var stack = [];
+  coins.forEach(function(coin, i) {
+    var diff = amount - coin.value;
+    if (diff > 0) {
+      var res = money(diff, coins.slice(i));
+      res.forEach(function(r) { r.unshift(coin); })
+      stack.push.apply(stack, res);
+    } else if (diff === 0) {
+      stack.push([coin]);
+    }
+  });
+  return stack;
+}
