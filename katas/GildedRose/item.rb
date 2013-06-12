fd4fcf0782ddf2efd9bc9e50d24ac0260b441782
Dominique Rose-Rosette <drose-rosette@af83.com>
2013-06-12 15:43:01 +0200

    Make Item comparable

--- katas/GildedRose/item.rb
+++ katas/GildedRose/item.rb
@@ -1,12 +1,15 @@
 class Item
 
   attr_accessor :name, :sell_in, :quality
-        
-  def initialize (name, sell_in, quality)
+
+  def initialize(name, sell_in, quality)
     @name = name
     @sell_in = sell_in
     @quality = quality
   end
 
+  def ==(other)
+    other.name == name && other.sell_in == sell_in && other.quality == quality
+  end
 
 end
