ea88fd827245b35481750ddb4ea372666367920b
Christophe Robillard <geek@robiweb.net>
2015-10-06 16:25:53 +0200

    Christophe avance

--- sierpinski/lib/pascal.js
+++ sierpinski/lib/pascal.js
@@ -1,12 +1,14 @@
-var pascal = function(ligne) {
-  var pascal=[]
-  if (ligne >= 1) {
-    pascal.push([1])
+var Pascal = {
+  triangle: function(ligne) {
+    var pascal=[]
+    if (ligne >= 1) {
+      pascal.push([1])
+    }
+    if (ligne >= 2) {
+      pascal.push([1,1])
+    }
+    return pascal
   }
-  if (ligne >= 2) {
-    pascal.push([1,1])
-  }
-  return pascal
 }
 
-module.exports = pascal
+module.exports = Pascal
--- sierpinski/spec/pascal_spec.js
+++ sierpinski/spec/pascal_spec.js
@@ -1,12 +1,12 @@
-var pascal = require('../lib/pascal')
+var Pascal = require('../lib/pascal')
 
 describe("Triangle de Pascal", function() {
   it('doit renvoyer comme premiere ligne 1', function() {
-    expect(pascal(1)).toEqual([[1]]);
+    expect(Pascal.triangle(1)).toEqual([[1]]);
   });
   
   it('doit renvoyer comme deuxieme ligne 1, 1', function() {
-    expect(pascal(2)).toEqual([[1],[1,1]]);
+    expect(Pascal.triangle(2)).toEqual([[1],[1,1]]);
   });
   
 })
