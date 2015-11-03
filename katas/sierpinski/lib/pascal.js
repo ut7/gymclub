4a1425ec3e2fce24f3e505182566b415a7cafb02
Christophe Robillard <geek@robiweb.net>
2015-11-03 15:39:49 +0100

    On redémarre le triangle de sierpinski

---
+++ katas/sierpinski/lib/pascal.js
@@ -0,0 +1,23 @@
+var construitLigne = function (previous_line) {
+  var line = [1];
+  var i;
+  for (i=0; i<= previous_line.length - 2; i++) {
+    line.push(previous_line[i] + previous_line[i+1]);
+  }
+  line.push(1); 
+  return line;
+};
+
+var genereTriangle = function (nb_lines) {
+  var triangle = [];
+  if (nb_lines >= 1) {
+    triangle.push([1]);
+  }
+
+  for (var i=2; i<= nb_lines; i++) {
+    triangle.push(construitLigne(triangle[i-2]));
+  }
+  return triangle;
+}
+
+module.exports.genereTriangle = genereTriangle
---
+++ katas/sierpinski/spec/pascal_spec.js
@@ -0,0 +1,21 @@
+var Pascal=require('../lib/pascal.js');
+
+describe('genereTriangleDePascal', function () {
+  it('doit renvoyer 1 pour un triangle à une ligne', function () {
+    expect(Pascal.genereTriangle(1)[0]).toEqual([1]);
+  });
+
+  it('doit renvoyer 1,1 pour un triangle à 2 lignes', function () {
+    expect(Pascal.genereTriangle(2)[1]).toEqual([1,1]);
+  });
+
+  it('doit renvoyer 1,2,1 pour un triangle à 3 lignes', function () {
+    expect(Pascal.genereTriangle(3)[2]).toEqual([1,2,1]);
+  });
+});
+
+describe('dessineTriangleDeSierpinski', function () {
+  it("affiche * pour un triangle d'une ligne", function () {
+    expect(Pascal.dessineTriangleDeSierpinski(1)).toEqual(['*']); 
+  });
+});
