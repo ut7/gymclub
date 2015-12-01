257e2fc8d4e5d1ef8d1d45ad3071c602e948a2d6
Christophe Robillard <geek@robiweb.net>
2015-12-02 00:57:25 +0100

    genere courbe sierpinski

--- katas/sierpinski-lsystem/lib/sierpinski.js
+++ katas/sierpinski-lsystem/lib/sierpinski.js
@@ -1,5 +1,29 @@
-transformA = function () {
+var transformA = function () {
   return 'B-A-B';
 }
 
-module.exports.transformA = transformA
+var transformB = function () {
+  return 'A+B+A';
+}
+
+var convert = function (sequence) {
+  return sequence.split('').map(function (character) {
+    switch(character) {
+      case 'A':
+        return transformA();
+        break;
+      case 'B':
+        return transformB();
+        break;
+      default:
+        return character;
+    }
+  }).join('');
+}
+
+var genereCourbeSierpinski = function (depth) {
+  if (depth === 0) return 'A';
+  return convert(genereCourbeSierpinski(depth - 1));
+}
+
+module.exports.genereCourbeSierpinski = genereCourbeSierpinski;
--- katas/sierpinski-lsystem/spec/sierpinski_spec.js
+++ katas/sierpinski-lsystem/spec/sierpinski_spec.js
@@ -1,7 +1,16 @@
 var Triangle = require('../lib/sierpinski.js');
 
-describe('transformA', function () {
-  it("renvoie 'B-A-B'", function() {
-    expect(Triangle.transformA()).toEqual('B-A-B');
+describe('genereCourbeSierpinski', function () {
+  it("renvoie 'A' pour une profondeur de 0", function() {
+    expect(Triangle.genereCourbeSierpinski(0)).toEqual('A');
+  });
+
+  it("renvoie 'B-A-B' pour une profondeur de 1", function() {
+    expect(Triangle.genereCourbeSierpinski(1)).toEqual('B-A-B');
+  });
+
+
+  it("renvoie 'A+B+A-B-A-B-A+B+A' pour une profondeur de 2", function() {
+    expect(Triangle.genereCourbeSierpinski(2)).toEqual('A+B+A-B-A-B-A+B+A');
   });
 });
