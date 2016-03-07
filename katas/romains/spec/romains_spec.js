48a5f39fe9406ea9288088c53dbecbb1e8c47685
Christophe Robillard <geek@robiweb.net>
2016-03-07 13:56:49 +0100

    reorder tests

--- katas/romains/spec/romains_spec.js
+++ katas/romains/spec/romains_spec.js
@@ -17,20 +17,8 @@ describe("Le convertisseur", function () {
         expect(convertis(1)).toEqual("I");
     });
    
-    it("convertit 5 en V", function () {
-        expect(convertis(5)).toEqual("V");
-    });
-   
-    it("convertit 6 en VI", function () {
-        expect(convertis(6)).toEqual("VI");
-    });
-   
     it("convertit 2 en II", function () {
         expect(convertis(2)).toEqual("II");
-    });
-
-    it("convertit 7 en VII", function () {
-        expect(convertis(7)).toEqual("VII");
     });
 
     it("convertit 3 en III", function () {
@@ -39,6 +27,19 @@ describe("Le convertisseur", function () {
 
     it("convertit 4 en IV", function () {
         expect(convertis(4)).toEqual("IV");
+    });
+
+
+    it("convertit 5 en V", function () {
+        expect(convertis(5)).toEqual("V");
+    });
+   
+    it("convertit 6 en VI", function () {
+        expect(convertis(6)).toEqual("VI");
+    });
+   
+    it("convertit 7 en VII", function () {
+        expect(convertis(7)).toEqual("VII");
     });
 
     it("convertit 8 en VIII", function () {
