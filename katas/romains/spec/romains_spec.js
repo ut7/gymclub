9bd7c7361f368b8222339d803c476e4ede52a0d6
Christophe Robillard <geek@robiweb.net>
2016-03-07 10:22:14 +0100

    add conversion for 3,4,8

--- katas/romains/spec/romains_spec.js
+++ katas/romains/spec/romains_spec.js
@@ -2,8 +2,14 @@ convertis = function (arabe) {
     if (arabe===1) {
         return "I";
     }
-    else if (arabe < 5) {
+    else if (arabe == 2) {
         return "II";
+    }
+    else if (arabe == 3) {
+        return "III";
+    }
+    else if (arabe == 4) {
+        return "IV";
     }
     else if (arabe > 5) {
         reste = arabe - 5;
@@ -32,6 +38,18 @@ describe("Le convertisseur", function () {
 
     it("convertit 7 en VII", function () {
         expect(convertis(7)).toEqual("VII");
+    });
+
+    it("convertit 3 en III", function () {
+        expect(convertis(3)).toEqual("III");
+    });
+
+    it("convertit 4 en IV", function () {
+        expect(convertis(4)).toEqual("IV");
+    });
+
+    it("convertit 8 en VIII", function () {
+        expect(convertis(8)).toEqual("VIII");
     });
 
    
