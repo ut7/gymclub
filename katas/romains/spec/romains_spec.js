7bb856cf09f5b5e3259cec0312efa086fceb80f3
Christophe Robillard <geek@robiweb.net>
2016-03-07 15:41:42 +0100

    move convertis function to src directory

--- katas/romains/spec/romains_spec.js
+++ katas/romains/spec/romains_spec.js
@@ -1,15 +1,3 @@
-convertis = function (arabe) {
-    if (arabe == 0) return "";
-
-    var table = [ [50,"L"], [40,"XL"], [10,"X"], [9,"IX"], [5,"V"], [4,"IV"], [1,"I"] ];
-    for (var index in table) {
-      if (arabe >= table[index][0]) {
-         return table[index][1] + convertis(arabe - table[index][0]);
-      };
-    }
-};
-
-
 describe("Le convertisseur", function () {
     it("convertit 1 en I", function () {
         expect(convertis(1)).toEqual("I");
