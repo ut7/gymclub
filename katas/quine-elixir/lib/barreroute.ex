f0b7a4385132a4d3553f08392fe96528811bc862
François de Metz <francois@2metz.fr>
2016-06-07 16:18:53 +0200

    On a fini. Et ca marche.

--- katas/quine-elixir/lib/barreroute.ex
+++ katas/quine-elixir/lib/barreroute.ex
@@ -1,11 +1,33 @@
 defmodule Barreroute do
   def tab do
     [
+      "defmodule Barreroute do",
+      "  def tab do",
+      "    [",
       "    ]",
       "  end",
       "",
+      "  def new_line do",
+      "    to_string([10])",
+      "  end",
+      "",
+      "  def double_quote do",
+      "    to_string([34])",
+      "  end",
+      "",
+      "  def anti_slash do",
+      "    to_string([92])",
+      "  end",
+      "",
+      "  def replace_chars(x) do",
+      "    String.replace(x, double_quote, anti_slash <> double_quote)",
+      "    |> String.replace(new_line, anti_slash <> to_string([110]))",
+      "  end",
+      "",
       "  def listing do",
-      "    Enum.join(tab(), \"\\n\")",
+      "    Enum.join(Enum.slice(tab(), 0..2), new_line) <> new_line <>",
+      "    Enum.map_join(tab(), to_string([44]) <> new_line, fn(x) -> to_string([32, 32, 32, 32, 32, 32]) <> double_quote <> replace_chars(x)  <> double_quote end) <> new_line <>",
+      "    Enum.join(Enum.slice(tab(), 3..-1), new_line)",
       "  end",
       "",
       "  def main do",
@@ -15,9 +37,27 @@ defmodule Barreroute do
     ]
   end
 
+  def new_line do
+    to_string([10])
+  end
+
+  def double_quote do
+    to_string([34])
+  end
+
+  def anti_slash do
+    to_string([92])
+  end
+
+  def replace_chars(x) do
+    String.replace(x, double_quote, anti_slash <> double_quote)
+    |> String.replace(new_line, anti_slash <> to_string([110]))
+  end
+
   def listing do
-    resultat = "      \"" <> List.last(tab) <> "\""
-    resultat <> Enum.join(tab(), "\n")
+    Enum.join(Enum.slice(tab(), 0..2), new_line) <> new_line <>
+    Enum.map_join(tab(), to_string([44]) <> new_line, fn(x) -> to_string([32, 32, 32, 32, 32, 32]) <> double_quote <> replace_chars(x)  <> double_quote end) <> new_line <>
+    Enum.join(Enum.slice(tab(), 3..-1), new_line)
   end
 
   def main do
--- katas/quine-elixir/test/barreroute_test.exs
+++ katas/quine-elixir/test/barreroute_test.exs
@@ -17,13 +17,33 @@ defmodule BarrerouteTest do
     assert Enum.at(list, length(list)-4) == "  def main do"
   end
 
-  test "cas du guillemet" do
-    list = String.split(Barreroute.listing(), "\n")
-    assert Enum.at(list, length(list)-7) == "    Enum.join(tab(), \"\\n\")"
-  end
-
   test "cas de la derniere ligne du tableau" do
     list = String.split(Barreroute.listing(), "\n")
-    assert Enum.at(list, length(list)-12) == "      \"end\""
+    assert Enum.at(list, length(list)-31) == "      \"end\""
+  end
+
+  test "cas de l'avant-derniere ligne du tableau" do
+    list = String.split(Barreroute.listing(), "\n")
+    assert Enum.at(list, length(list) - 32) == "      \"  end\","
+  end
+
+  test "cas de listing" do
+    list = String.split(Barreroute.listing(), "\n")
+    assert Enum.at(list, length(list) - 37) == "      \"    Enum.join(Enum.slice(tab(), 3..-1), new_line)\","
+  end
+
+  test "cas du debut" do
+    list = String.split(Barreroute.listing(), "\n")
+    assert Enum.at(list, 0) == "defmodule Barreroute do"
+  end
+
+  test "cas de tab" do
+    list = String.split(Barreroute.listing(), "\n")
+    assert Enum.at(list, 1) == "  def tab do"
+  end
+
+  test "cas du debut du tableau" do
+    list = String.split(Barreroute.listing(), "\n")
+    assert Enum.at(list, 2) == "    ["
   end
 end
