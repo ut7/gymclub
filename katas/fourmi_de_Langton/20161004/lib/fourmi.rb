a4f9917c3f066811ba7549f400e3a18c714ee49f
Christophe Robillard <geek@robiweb.net>
2016-10-25 16:19:33 +0200

    fourmi avance depuis une case noire

--- katas/fourmi_de_Langton/20161004/lib/fourmi.rb
+++ katas/fourmi_de_Langton/20161004/lib/fourmi.rb
@@ -8,9 +8,19 @@ class Fourmi
 
   def avance(plateau)
     case_plateau = plateau.case(self.ligne, self.colonne)
+    case_plateau.couleur == :blanc ? avance_depuis_case_blanche(case_plateau) : avance_depuis_case_noire(case_plateau)
+  end
+
+  def avance_depuis_case_blanche(case_plateau)
     self.direction = direction_case_blanche
     send("tourne_#{direction}".to_sym)
     case_plateau.couleur = :noir
+  end
+
+  def avance_depuis_case_noire(case_plateau)
+    self.direction = direction_case_noire
+    send("tourne_#{direction}".to_sym)
+    case_plateau.couleur = :blanc
   end
 
   def direction_index
@@ -19,6 +29,10 @@ class Fourmi
 
   def direction_case_blanche
     DIRECTIONS[(direction_index + 1) % 4]
+  end
+
+  def direction_case_noire
+    DIRECTIONS[(direction_index - 1) % 4]
   end
 
   def tourne_droite
--- katas/fourmi_de_Langton/20161004/spec/fourmi_spec.rb
+++ katas/fourmi_de_Langton/20161004/spec/fourmi_spec.rb
@@ -30,4 +30,18 @@ describe Fourmi do
     expect(plateau.case(1,1).couleur).to eq(:noir)
     expect(plateau.case(2,1).couleur).to eq(:blanc)
   end
+
+  it "avance depuis une case noire regardant vers le haut" do
+    plateau = Plateau.new(3,3)
+    plateau.case(1,1).couleur = :noir
+    fourmi = Fourmi.new
+    fourmi.direction = :haut
+
+    plateau.place(fourmi, 1,1)
+    fourmi.avance(plateau)
+    expect(fourmi.ligne).to eq(1)
+    expect(fourmi.colonne).to eq(0)
+    expect(plateau.case(1,1).couleur).to eq(:blanc)
+
+  end
 end
