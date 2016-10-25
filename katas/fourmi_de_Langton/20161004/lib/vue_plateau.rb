34a434a8e1b0fe837efc74663f6b549002cee6e0
Christophe Robillard <geek@robiweb.net>
2016-10-25 13:25:13 +0200

    affiche les cases noires

--- katas/fourmi_de_Langton/20161004/lib/vue_plateau.rb
+++ katas/fourmi_de_Langton/20161004/lib/vue_plateau.rb
@@ -5,10 +5,19 @@ class VuePlateau
 
   def grille
     chaine_grille = ""
-    @plateau.lignes.times do
-      chaine_grille += "| " * @plateau.colonnes + "|\n"
+    @plateau.lignes.times.each do |ligne|
+      @plateau.colonnes.times.each do |colonne|
+        chaine_grille += affiche_case(ligne,colonne)
+      end
+      chaine_grille += "|\n"
     end
     chaine_grille
+  end
+
+  def affiche_case(ligne, colonne)
+    case_plateau = @plateau.case(ligne,colonne)
+    correspondance = {:blanc => ' ', :noir => 'x'}
+    "|#{correspondance[case_plateau.couleur]}"
   end
 
   def affiche
--- katas/fourmi_de_Langton/20161004/spec/vue_plateau_spec.rb
+++ katas/fourmi_de_Langton/20161004/spec/vue_plateau_spec.rb
@@ -6,4 +6,11 @@ describe VuePlateau do
     vue_plateau = VuePlateau.new(plateau)
     expect(vue_plateau.grille).to eq("| | | |\n| | | |\n| | | |\n")
   end
+
+  it "affiche x dans une case noire" do
+    plateau = Plateau.new(1,1)
+    plateau.case(0,0).couleur = :noir
+    vue_plateau = VuePlateau.new(plateau)
+    expect(vue_plateau.grille).to eq("|x|\n")
+  end
 end
