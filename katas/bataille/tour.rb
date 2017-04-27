11191ce397a773f0857037396f074030bbcdf20d
krichtof <geek@robiweb.net>
2017-04-27 14:35:31 +0200

    gere le cas où les 2 cartes sont identiques

--- katas/bataille/tour.rb
+++ katas/bataille/tour.rb
@@ -1,24 +1,37 @@
 module Bataille
   class Tour
-    attr_reader :main1, :main2
+    attr_reader :main1, :main2, :levee
 
     def initialize(main1, main2)
       @main1 = main1
       @main2 = main2
+      @levee = []
     end
 
     def suivant!
+      gagnant = main_gagnante
+      update_levee!
+      distribue_levee!(gagnant) if gagnant
+    end
+
+    private
+
+    def main_gagnante
+      if main1.last > main2.last
+        main1
+      elsif main1.last < main2.last
+        main2
+      end
+    end
+
+    def update_levee!
       haut1 = main1.pop
       haut2 = main2.pop 
+      levee.push(haut1, haut2)
+    end
 
-      if haut1 > haut2
-        main1.unshift(haut1)
-        main1.unshift(haut2)
-      else
-        main2.unshift(haut1)
-        main2.unshift(haut2)
-      end
-
+    def distribue_levee!(main)
+      main.unshift(levee.pop) while levee.any?
     end
   end
 end
--- katas/bataille/tour_spec.rb
+++ katas/bataille/tour_spec.rb
@@ -22,6 +22,24 @@ describe Bataille::Tour do
   end
 
   it "reordonne les 2 mains avec les 2 cartes de haut de pile identiques" do
-    main1 = [ 3, 11 ]
+    main1 = [ 2, 3 , 11 ]
+    main2 = [ 5, 6 , 11 ]
+    tour = Bataille::Tour.new(main1, main2)
+    tour.suivant!
+    expect(tour.levee).to eq([11,11])
+    expect(tour.main1).to eq([2,3])
+    expect(tour.main2).to eq([5,6])
+  end
+
+  it "reordonne les 2 mains avec une levee" do
+    main1 = [ 2, 3 , 11 ]
+    main2 = [ 5, 6 , 11 ]
+    tour = Bataille::Tour.new(main1, main2)
+    tour.suivant!
+    tour.suivant!
+    expect(tour.main1.last).to eq(2)
+    expect(tour.main2.last).to eq(5)
+    expect(tour.main2.slice(0,4) - [ 3,6,11,11 ]).to eq([])
+    expect([ 3,6,11,11 ] - tour.main2.slice(0,4) ).to eq([])
   end
 end
