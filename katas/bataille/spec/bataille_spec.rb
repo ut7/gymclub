48dfe22987964e8ef6800431acf3aecafd57ee50
krichtof <geek@robiweb.net>
2017-05-23 10:02:23 +0200

    move spec in spec directory

--- katas/bataille/spec/bataille_spec.rb
+++ katas/bataille/spec/bataille_spec.rb
@@ -3,14 +3,14 @@ require './bataille'
 
 RSpec.describe Bataille do
   it "renvoie le jeu gagnant" do
-    bob = Joueur.new([10])
-    alice = Joueur.new([9])
+    bob = Joueur.new(jeu: [10])
+    alice = Joueur.new(jeu: [9])
     expect(Bataille::result_war(bob, alice)).to eq(bob)
   end
 
   it "renvoie le jeu gagnant" do
-    bob = Joueur.new([10])
-    alice = Joueur.new([9])
+    bob = Joueur.new(jeu: [10])
+    alice = Joueur.new(jeu: [9])
     expect(Bataille::result_war(alice, bob)).to eq(bob)
   end
 end
