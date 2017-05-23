66effe36bae5d3caf388adb45f1d71a48649036e
krichtof <geek@robiweb.net>
2017-05-23 12:13:16 +0200

    ajoute correctement dans la levee en cas de bataille

--- katas/bataille/spec/tour_spec.rb
+++ katas/bataille/spec/tour_spec.rb
@@ -1,18 +1,18 @@
 require 'rspec'
 require './bataille'
 
-describe Bataille::Tour do
+describe Tour do
   it "initialise un tour avec 2 joueurs" do
     bob = double("bob", jeu: [2,3,11])
     alice = double("alice", jeu: [3,4,5])
-    expect(Bataille::Tour.new(bob, alice)).to be_truthy
+    expect(Tour.new(bob, alice)).to be_truthy
   end
 
 
   it "reordonne les 2 mains avec la carte en haut de pile de la première main > à celle de la deuxième main" do
     bob = Joueur.new(jeu: [2,3,11])
     alice = Joueur.new(jeu: [3,4,5])
-    tour = Bataille::Tour.new(bob, alice)
+    tour = Tour.new(bob, alice)
     tour.suivant!
     expect(alice.jeu.size).to eq(2)
     expect(alice.jeu.last).to eq(4)
@@ -24,22 +24,10 @@ describe Bataille::Tour do
   it "reordonne les 2 mains avec les 2 cartes de haut de pile identiques" do
     bob = Joueur.new(jeu: [2,3,11])
     alice = Joueur.new(jeu: [5,6,11])
-    tour = Bataille::Tour.new(bob, alice)
-    tour.suivant!
-    expect(tour.levee).to eq([11,11])
-    expect(bob.jeu).to eq([2,3])
-    expect(alice.jeu).to eq([5,6])
-  end
-
-  it "reordonne les 2 mains avec une levee" do
-    bob = Joueur.new(jeu: [ 2, 3 , 11 ], strategie: StrategieDeRangementAleatoire)
-    alice = Joueur.new(jeu:[ 5, 6 , 11 ])
-    tour = Bataille::Tour.new(bob, alice)
+    tour = Tour.new(bob, alice)
     tour.suivant!
     tour.suivant!
-    expect(bob.jeu.last).to eq(2)
-    expect(alice.jeu.last).to eq(5)
-    expect(alice.jeu.slice(0,4) - [ 3,6,11,11 ]).to eq([])
-    expect([ 3,6,11,11 ] - alice.jeu.slice(0,4) ).to eq([])
+    expect(bob.jeu).to eq([])
+    expect(alice.jeu.sort).to eq([2,5,3,6,11,11].sort)
   end
 end
--- katas/bataille/tour.rb
+++ katas/bataille/tour.rb
@@ -1,55 +1,54 @@
-module Bataille
-  class Tour
-    attr_reader :joueur1, :joueur2, :levee
+class Tour
+  attr_reader :joueur1, :joueur2, :levee
 
-    def initialize(joueur1, joueur2)
-      @joueur1 = joueur1
-      @joueur2 = joueur2
-      @levee = []
-    end
+  def initialize(joueur1, joueur2)
+    @joueur1 = joueur1
+    @joueur2 = joueur2
+    @levee = []
+  end
 
-    def suivant!
-      gagnant = joueur_gagnant
+  def suivant!
+    gagnant = joueur_gagnant
+    update_levee!
+    if gagnant
+      distribue_levee!(gagnant)
+    else
       update_levee!
-      if gagnant
-        distribue_levee!(gagnant)
-      end
-    end
-
-    private
-
-    def joueur_gagnant
-      hash = { jeu1.last => joueur1, jeu2.last => joueur2 }
-      hash[ carte_gagnante(jeu1.last, jeu2.last) ]
-    end
-
-    def carte_gagnante(carte1, carte2)
-      if carte1 > carte2
-        carte1
-      elsif carte1 < carte2
-        carte2
-      end
-    end
-
-    def jeu1
-      joueur1.jeu
-    end
-
-    def jeu2
-      joueur2.jeu
-    end
-
-    def update_levee!
-      haut1 = jeu1.pop
-      haut2 = jeu2.pop 
-      levee.push(haut1, haut2)
-    end
-
-    def distribue_levee!(joueur)
-      joueur.strategie.ordonne!(levee) if joueur.strategie
-      joueur.jeu.unshift(levee)
-      joueur.jeu.flatten!
-      @levee = []
     end
   end
+
+  private
+
+  def joueur_gagnant
+    hash = { jeu1.last => joueur1, jeu2.last => joueur2 }
+    hash[ carte_gagnante(jeu1.last, jeu2.last) ]
+  end
+
+  def carte_gagnante(carte1, carte2)
+    if carte1 > carte2
+      carte1
+    elsif carte1 < carte2
+      carte2
+    end
+  end
+
+  def jeu1
+    joueur1.jeu
+  end
+
+  def jeu2
+    joueur2.jeu
+  end
+
+  def update_levee!
+    levee.push(jeu1.pop, jeu2.pop)
+  end
+
+  def distribue_levee!(joueur)
+    joueur.strategie.ordonne!(levee) if joueur.strategie
+    joueur.jeu.unshift(levee)
+    joueur.jeu.flatten!
+    @levee = []
+  end
+
 end
